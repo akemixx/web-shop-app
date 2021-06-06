696cd545dc23fa44638ef09805f2935b41929a90
Valeriia Shaposhnikova <shaposhnikova.valeri@gmail.com>
2021-06-06 21:47:09 +0300

    Primary backend created. Success, loading and message boxes added. Connected frontend and backend.

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serve at http://localhost:${PORT}`);
+});
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -41,7 +41,7 @@ export default function ProductPage(props) {
                                     <div>
                                         {product.countInStock > 0
                                             ? <span className="success">In Stock</span>
-                                            : <span className="error">Unavailable</span>}
+                                            : <span className="danger">Unavailable</span>}
                                     </div>
                                 </div>
                             </li>
